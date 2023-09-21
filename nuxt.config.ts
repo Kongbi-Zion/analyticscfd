649f81fa4bba09d37a5e3cbfa16ef8b464e574fa
Kongbi zion nfor <kongbizion3@gmail.com>
2023-09-21 20:28:33 +0100

    updated config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,24 @@ export default defineNuxtConfig({
   // devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   plugins: [{ src: "plugins/amplify.js", mode: "client" }],
+  vite: {
+    resolve: {
+      alias: {
+        "./runtimeConfig": "./runtimeConfig.browser",
+      },
+    },
+
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
+
+    // temp-fix for dev, it breaks build for now (see: https://github.com/nuxt/framework/issues/4916)
+    define: {
+      "window.global": {},
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
